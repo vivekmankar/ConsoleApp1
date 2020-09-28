71f4fde7a60574067020ef975102655b5c56a775
Vivek Mankar <vivekmankar@usf.edu>
2020-09-28 17:16:12 -0400

    Add project files.

---
+++ ConsoleApp1/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Diagnostics;
+
+namespace ConsoleApp1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+            Console.ReadKey();
+            Debug.WriteLine("Hello World!");
+
+            int x;
+            int y;
+            x = 3;
+            y = 4;
+            int sum = x + y;
+            Console.WriteLine("x + Y is" + sum);
+            Debug.WriteLine("x + Y is" + sum);
+        }
+    }
+}
